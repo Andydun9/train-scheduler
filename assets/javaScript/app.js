edaa5cbe6c06f69e221db346e76e8eb9ae4bd0c0
Your Name <you@example.com>
2018-06-17 18:25:13 -0700

    form is pushing content to firebase. Need to now work on adding timer function

---
+++ assets/javaScript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyA4P1Lp7N7Nvs8eo0IXgYCaxqcTUnWR7Fc",
+    authDomain: "train-time-7cdee.firebaseapp.com",
+    databaseURL: "https://train-time-7cdee.firebaseio.com",
+    projectId: "train-time-7cdee",
+    storageBucket: "",
+    messagingSenderId: "1023928771689"
+  };
+  firebase.initializeApp(config);
